bd41f9eb11e1e12765b5339ca96be38f1442a0e7
lining9717 <980994630@qq.com>
2020-02-09 15:21:49 +0800

    some modify

--- DstarLite/dstarlite.cpp
+++ DstarLite/dstarlite.cpp
@@ -312,28 +312,4 @@ int DStarLite::move_to_goal(std::vector<Node> &path)
     return HAVE_PATH;
 }
 
-int main(int argc, char const *argv[])
-{
-    DStarLite dsl;
-    if (!dsl.init("./map1.txt", 4))
-        return 0;
 
-    std::vector<Node> path;
-    int status = dsl.move_to_goal(path);
-    if (status == HAVE_PATH)
-    {
-        std::cout << "Final Path:[Start=";
-        int length = path.size();
-        for (int i = 0; i < length; i++)
-        {
-            std::cout << "(" << path[i].x << "," << path[i].y << ")";
-            if (i + 1 != length)
-                std::cout << "->";
-        }
-        std::cout << "=Goal]" << std::endl;
-    }
-    else
-        std::cout << "No Path!" << std::endl;
-
-    return 0;
-}
--- DstarLite/main.cpp
+++ DstarLite/main.cpp
@@ -3,12 +3,25 @@
 int main(int argc, char const *argv[])
 {
     DStarLite dsl;
-    dsl.init("/home/ln/WorkSpace/c++/pathplanning/DstarLite/map.txt", 2);
-    std::cout << "The graph (A=Start, Z=Goal):" << std::endl;
-    dsl.draw_map();
+    if (!dsl.init("./map1.txt", 4))
+        return 0;
+
     std::vector<Node> path;
-    dsl.move_to_goal(path);
-    std::cout << "Path taken (@ symbols):" << std::endl;
-    dsl.draw_map(path);
+    int status = dsl.move_to_goal(path);
+    if (status == HAVE_PATH)
+    {
+        std::cout << "Final Path:[Start=";
+        int length = path.size();
+        for (int i = 0; i < length; i++)
+        {
+            std::cout << "(" << path[i].x << "," << path[i].y << ")";
+            if (i + 1 != length)
+                std::cout << "->";
+        }
+        std::cout << "=Goal]" << std::endl;
+    }
+    else
+        std::cout << "No Path!" << std::endl;
+
     return 0;
 }
